198d0323c77708c37462542616f7e68bbf20d728
Nastya <shebeko.aiu@phystech.edu>
2023-04-24 16:13:37 +0300

    tests

--- pdp-tests.c
+++ pdp-tests.c
@@ -1,4 +1,7 @@
 #include "pdp-11.c"
+#include <stdarg.h>
+#include "pdp_command.c"
+# include <assert.h>
 
 void test_mem()
 {
@@ -50,3 +53,111 @@ void test_mem()
     
 }
 
+#define DEBUG 1
+#define MORE_DEBUG 2
+
+int debug_level = DEBUG;
+
+/*void trace(int dbg_lvl, char * format, ...) 
+{//1/2 x3
+	if (dbg_lvl != debug_level)
+		return;
+	va_list argptr;
+	va_start (argptr, format);
+	vprintf(format, argptr);
+	va_end(argptr);	
+}
+*/
+
+
+// тест, что мы правильно определяем команды mov, add, halt
+void test_parse_mov()
+{
+            //trace(TRACE, __FUNCTION__);
+    Command cmd = parse_cmd(0017654);
+    assert(strcmp(cmd.name, "mov"));
+            //trace(TRACE, " ... OK\n");
+    printf(" ... OK\n");
+}
+
+// тест, что мы разобрали правильно аргументы ss и dd в mov R5, R3
+void test_mode0()
+{
+            //trace(TRACE, __FUNCTION__);
+    reg[3] = 12;    // dd
+    reg[5] = 34;    // ss
+    Command cmd = parse_cmd(0010503); //????
+    assert(ss.val == 34);
+    assert(ss.a == 5);
+    assert(dd.val == 12);
+    assert(dd.a == 3);
+            //trace(TRACE, " ... OK\n");
+    printf(" ... OK\n");
+}
+// тест, что mov и мода 0 работают верно в mov R5, R3
+ void test_mov()
+{
+            //trace(TRACE, __FUNCTION__);
+    reg[3] = 12;    // dd
+    reg[5] = 34;    // ss
+    Command cmd = parse_cmd(0010503);
+    cmd.do_command();
+    assert(reg[3] = 34);
+    assert(reg[5] = 34);
+            //trace(TRACE, " ... OK\n");
+    printf(" ... OK\n");
+}
+
+void test_mode1_toreg()
+{
+            //trace(TRACE, __FUNCTION__)
+    // setup
+    reg[3] = 12;    // dd
+    reg[5] = 0200;  // ss
+    w_write(0200, 34);
+
+
+    Command cmd = parse_cmd(0011503);
+
+
+    assert(ss.val == 34);
+    assert(ss.a == 0200);
+    assert(dd.val == 12);
+    assert(dd.a == 3);
+
+
+    cmd.do_command();
+
+    assert(reg[3] = 34);
+    // проверяем, что значение регистра не изменилось
+    assert(reg[5] = 0200);
+
+            //trace(TRACE, " ... OK\n");
+    printf(" ... OK\n");
+}
+
+void test_mode2_toreg()
+{
+    reg[3] = 12;    // dd
+    reg[5] = 0200;  // ss
+    w_write(0200, 34);
+
+
+    Command cmd = parse_cmd(0011503);
+
+
+    assert(ss.val == 34);
+    assert(ss.a == 0200);
+    assert(dd.val == 12);
+    assert(dd.a == 3);
+
+    cmd.do_command();
+
+    assert(reg[3] = 34);
+
+    // проверяем, что значение регистра увеличилось на 2
+    assert(reg[5] = 0202);
+
+            //trace(TRACE, " ... OK\n");
+    printf(" ... OK\n");
+}
