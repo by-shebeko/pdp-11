db8e305eb875c70a07662664351b7cc19016cb5f
Nastya <shebeko.aiu@phystech.edu>
2023-04-24 16:18:24 +0300

    changed

--- pdp-run.c
+++ pdp-run.c
@@ -7,6 +7,8 @@ word reg[8] ;        //registers R0, R1, ... R7. R7 program counter
 
 void run()
 {
+    printf("\nRunning \n");
+    int i = 0;
     pc = 01000;         //восьмеричное начало адреса
 
     while(1)
@@ -15,23 +17,31 @@ void run()
         printf("%06o %06o: ", pc, w);
         pc += 2;                        // move on 2, cause only odd numbs as adresses
 
-
-
         if (w == 0)
         {
             printf("halt ");
             do_halt();
         }
 
-        for ()
-
-        if ((w & mask) == opcode);
-
-        else if
+        for (i = 0; ; i++)
         {
+            Command cmd = list[i];
+
+            if ((w & cmd.mask) == cmd.opcode)
+            {
+                printf ("%s ", cmd.name);
+
+                if (cmd.params & HAS_SS)
+                    ss = get_mr (w >> 6); //тк формат записи двоичного числа ... |ssssss|dddddd|
+
+                if (cmd.params & HAS_DD)
+                    dd = get_mr (w);
+
+            }
 
         }
         
     }
 }
+
 
