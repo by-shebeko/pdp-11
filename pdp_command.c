fb48ce3d898f99f4c851c4ef2ce5e41e8a03c0db
Nastya <shebeko.aiu@phystech.edu>
2023-04-24 16:23:43 +0300

    changed

--- pdp_command.c
+++ pdp_command.c
@@ -1,29 +1,57 @@
 #include <stdio.h>
 #include "pdp-mem.c"
+#include "registres.c"
 
-void do_mov () {}
-void do_add () {}
-void do_nothing () {}
-void do_halt () {
-    printf("THE END!!!\n");
-    exit(0);
-}
+#define NO_PARAMS 0
+#define HAS_DD 1
+#define HAS_SS 2
 
-typedef struct {            // struct for commands for easier if cycle
+void do_mov ();
+void do_add ();
+void do_nothing ();
+void do_halt ();
+
+/*typedef struct {            // struct for commands for easier if cycle
     word mask;
     word opcode;
     char * name;
     void (*do_func)(void);
 } Command;
+*/
 
-Command cmd[] = {
-    {0170000, 0010000, "mov", do_mov},
-    {0170000, 0060000, "add", do_add},
-    {0177777, 0000000, "halt", do_halt}
+typedef struct {
+    word mask;
+    word opcode;
+    char * name;
+    void (*do_command)(void);
+    char params;    // в 1 байте кодируем наличие разных типов аргументов
+} Command;
+
+Command list[] = {
+    {0170000, 0010000, "mov", do_mov, HAS_SS | HAS_DD},
+    {0170000, 0060000, "add", do_add, HAS_SS | HAS_DD},
+    {0177777, 0000000, "halt", do_halt, NO_PARAMS}
 };
 
-void do_mov() 
+void do_halt () 
 {
-    w_write(dd.a, (ss.val & 0xFFFF)); //dd ss are ending parts 
-    return;
+    printf("THE END!!!\n");
+    exit(0);
 }
+void do_mov()
+{
+    // значение аргумента ss пишем по адресу аргумента dd
+    w_write(dd.a, ss.val);
+}
+void do_add()
+{
+    // сумму значений аргументов ss и dd пишем по адресу аргумента dd
+    w_write(dd.a, ss.val + dd.val);
+}
+
+void do_unknown (Command res)
+{}
+
+
+
+
