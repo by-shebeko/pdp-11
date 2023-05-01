1816e0ca40f47585d4fc974e5dfaf7c2d73aac22
Nastya <shebeko.aiu@phystech.edu>
2023-05-01 13:46:56 +0300

    load_file fixed

--- pdp_load_file.c
+++ pdp_load_file.c
@@ -14,7 +14,7 @@ void load_file(const char* filename)
 	
 	while (fscanf(fin ,"%4hx %4hx", &mem_adr, &N) > 1) 
     {
-		for (; i < N; i++) 
+		for (i = 0x0000; i < N; i++) 
         {
 			fscanf(fin ,"%2hhx", &b); //считаываем значение
 			b_write(mem_adr + i, b);  //записываем в массив с данными и двигаем указатель
